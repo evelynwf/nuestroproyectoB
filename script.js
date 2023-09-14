9d70661d34c852133eb16c11dff91ecfc9eb0645
yaebingi <141787497+yaebingi@users.noreply.github.com>
2023-09-13 22:11:43 -0300

    Update script.js
    puse el alert de mi sección

---
+++ script.js
@@ -0,0 +1,4 @@
+/* alert del acerca de*/
+window.onload = function() {
+  alert("¡Bienvenido/as a la página web de emprendedores! Aquí encontrarás información sobre el equipo. Esperamos poder ayudarte.");
+};
