ee058e8157f77c7c1af8674828ac3abb4ed1ecbf
evelynwf <evelynwafigue@gmail.com>
2023-09-14 20:01:01 -0300

    Esto

---
+++ script.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Por favor, ingresa tu nombre:");
+let apellido = prompt("Por favor, ingresa tu apellido:");
+
+let mensaje = "Bienvenida " + nombre + " " + apellido + ", estas son nuestras recomendaciones de cursos.";
+
+alert(mensaje);
