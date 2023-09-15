f69b96875e6aef26d523f1ec5fc7ab05947d47e4
evelynwf <evelynwafigue@gmail.com>
2023-09-14 23:55:33 -0300

    Newcambios

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 let nombre = prompt("Por favor, ingresa tu nombre:");
 let apellido = prompt("Por favor, ingresa tu apellido:");
 
 let mensaje = "Bienvenida " + nombre + " " + apellido + ", estas son nuestras recomendaciones de cursos.";
 
 alert(mensaje);
+=======
+/* alert del acerca de*/
+window.onload = function() {
+  alert("¡Bienvenido/as a la página web de emprendedores! Aquí encontrarás información sobre el equipo. Esperamos poder ayudarte.");
+};
+>>>>>>> 549e0194767c96590385ae719698ae2b6316e408
