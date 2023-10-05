118fc6898c057922e7db15c299e4aff53eee7772
evelynwf <evelynwafigue@gmail.com>
2023-10-05 20:32:35 -0300

    va de nuevo

---
+++ cursos.js
@@ -0,0 +1,46 @@
+let datosJSON = {
+    cursos: [
+      {
+        nombre: "Capacitar Argentina",
+        imagen: "capacitarcursos.jpg",
+        enlace: "https://www.argentina.gob.ar/produccion/capacitar"
+      },
+      {
+        nombre: "Cumbre Neuquén Capital",
+        imagen: "img3cursos.jpg",
+        enlace: "https://cumbre.neuquen.gov.ar/cursos"
+      },
+      {
+        nombre: "Municipalidad de Neuquén",
+        imagen: "img2cursos.jpg",
+        enlace: "https://web.muninqn.gov.ar/course/index.php?categoryid=3"
+      },
+      {
+        nombre: "Academia BA",
+        imagen: "img1cursos.jpg",
+        enlace: "https://buenosaires.gob.ar/desarrolloeconomico/emprendedores/capacitacion-e-incubadoras/academia-ba-emprende"
+      }
+    ]
+  };
+  
+  function agregarCursosDesdeJSON() {
+    const listaCursos = document.getElementById('lista-cursos');
+    
+    datosJSON.cursos.forEach(curso => {
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      img.src = curso.imagen;
+      img.alt = curso.nombre;
+      const a = document.createElement('a');
+      a.target = '_blank';
+      a.href = curso.enlace;
+      a.textContent = curso.nombre;
+  
+      li.appendChild(img);
+      li.appendChild(a);
+      listaCursos.appendChild(li);
+    });
+  }
+  
+  agregarCursosDesdeJSON();
+  
