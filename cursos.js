cb02b4b8da70fe5df0cde8eba13503483228efdd
evelynwf <evelynwafigue@gmail.com>
2023-10-08 16:46:29 -0300

    consola otra vez

--- cursos.js
+++ cursos.js
@@ -1,32 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let datosJSON = {
-      cursos: [
-        {
-          nombre: "Capacitar Argentina",
-          imagen: "capacitarcursos.jpg",
-          enlace: "https://www.argentina.gob.ar/produccion/capacitar"
-        },
-        {
-          nombre: "Cumbre Neuquén Capital",
-          imagen: "img3cursos.jpg",
-          enlace: "https://cumbre.neuquen.gov.ar/cursos"
-        },
-        {
-          nombre: "Municipalidad de Neuquén",
-          imagen: "img2cursos.jpg",
-          enlace: "https://web.muninqn.gov.ar/course/index.php?categoryid=3"
-        },
-        {
-          nombre: "Academia BA",
-          imagen: "img1cursos.jpg",
-          enlace: "https://buenosaires.gob.ar/desarrolloeconomico/emprendedores/capacitacion-e-incubadoras/academia-ba-emprende"
-        }
-      ]
-    };
-  
-    function agregarCursosDesdeJSON() {
-      const listaCursos = document.getElementById('lista-cursos');
-  
+  let datosJSON = {
+    cursos: [
+      {
+        nombre: "Capacitar Argentina",
+        imagen: "capacitarcursos.jpg",
+        enlace: "https://www.argentina.gob.ar/produccion/capacitar"
+      },
+      {
+        nombre: "Cumbre Neuquén Capital",
+        imagen: "img3cursos.jpg",
+        enlace: "https://cumbre.neuquen.gov.ar/cursos"
+      },
+      {
+        nombre: "Municipalidad de Neuquén",
+        imagen: "img2cursos.jpg",
+        enlace: "https://web.muninqn.gov.ar/course/index.php?categoryid=3"
+      },
+      {
+        nombre: "Academia BA",
+        imagen: "img1cursos.jpg",
+        enlace: "https://buenosaires.gob.ar/desarrolloeconomico/emprendedores/capacitacion-e-incubadoras/academia-ba-emprende"
+      }
+    ]
+  };
+
+  function agregarCursosDesdeJSON() {
+    const listaCursos = document.getElementById('lista-cursos');
+
+    if (listaCursos) {
       datosJSON.cursos.forEach(curso => {
         const li = document.createElement('li');
         const img = document.createElement('img');
@@ -36,13 +37,13 @@ document.addEventListener('DOMContentLoaded', function () {
         a.target = '_blank';
         a.href = curso.enlace;
         a.textContent = curso.nombre;
-  
+
         li.appendChild(img);
         li.appendChild(a);
         listaCursos.appendChild(li);
       });
     }
-  
-    agregarCursosDesdeJSON();
-  });
-  
+  }
+
+  agregarCursosDesdeJSON();
+}); 
